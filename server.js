564ebeb8568e508c95276e0953258d7c5b82264b
Betelgeuse1 <avanalos@gmail.com>
2020-11-08 04:15:32 +0100

    Supprimer mon taf pour recommencer de zéro. Un plaisir.

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 const WebSocket = require('websocket').server
 const fs = require('fs')
-const DBWrapper = require('./src/js/database/MySQLWrapper.js')
+const mysql = require('mysql')
+const md5 = require('md5')
 
 const app = express()
+const connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'thomas',
+	password: '10021316',
+	database: 'vancode'
+})
+
+// Parse le JSON reçut sur les endpoints
+app.use(express.json())
 
 app.all('/api', (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -19,12 +29,56 @@ app.get('/', (req, res) => {
 	})
 })
 
-app.post('/api', (req, res) => {
-	res.send(JSON.stringify({
-		user: 'AlexLeBG',
-		password: 'JeSuisSecret',
-		token: 'hfjhHJDH6pA798uidsihiU768D7yd_kdkE'
-	}))
+app.post('/api/login', (req, res) => {
+	const username = req.body.userName
+	const password = req.body.password
+
+	connection.query("SELECT * FROM users WHERE username = ? LIMIT 1", [username],
+		(error, results, fields) => {
+			try {
+				// On fait nos tests sur les potentielles erreurs
+				if (error) throw error
+				if (results.length === 0) throw new Error("No username match")
+				if (username === undefined || password === undefined) throw new Error("Data wasn't received")
+				
+				// Puisqu'on a précisé une limite, on n'est sûr de n'avoir
+				// qu'un seul résultat
+				const dbUser = results[0]
+
+				// On vérifie que les hashs des mdp correspondent
+				const hashPass = md5(password)
+
+				if (hashPass !== dbUser.password) throw new Error("Passwords don't match with database")
+
+				// Si on est arrivé ici, alors le compte correspond à celui de la bd
+				// On renvoie donc une requête 200 (OK) ainsi que le token de connexion
+				const responseData = JSON.stringify(
+					{
+						token: "fjjijjfkdju8YC8CHHCO_d_hcO98CI"
+					}
+				)
+
+				res.writeHead(200, {'Content-Type': 'application/json'})
+				res.write(responseData)
+
+			} catch(error) {
+				// On arrive ici si une erreur a été renvoyée (throw)
+				// On répond à la FE avec une liste
+				const errorData = JSON.stringify(
+					{
+						error: error
+					}
+				)
+
+				res.writeHead(400, {'Content-Type': 'application/json'})
+				res.write(errorData)
+
+			} finally {
+				// Quoiqu'il arrive on doit terminer la requête
+				res.end()
+			}
+		}
+	)
 })
 
 const port = process.env.PORT || 3000
